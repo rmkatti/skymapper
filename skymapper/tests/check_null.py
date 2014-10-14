fe488397b844212f03123536ae23c9d80f0168b0
Raj Katti <rmkatti@gmail.com>
2014-10-13 20:56:10 -0700

    Produced allsky no-coverage map

--- skymapper/tests/check_null.py
+++ skymapper/tests/check_null.py
@@ -1,23 +1,32 @@
-import numpy as np
-from numpy import pi
-import matplotlib.pyplot as plt
-import healpy as hp
-import skymapper.gen_maps.SkyMap as SkyMap
+from skymapper.visualize.plot_points import moll_plot_points
+
 from skymapper.analyze.an_tools import find_missed
-from skymapper.visualize.plot_points import rad_plot_points
+import skymapper.gen_maps.SkyMap as SkyMap
+import healpy as hp
+
 import itertools
 import cPickle as pickle
+import time
+import numpy as np
+from numpy import pi
+
 
 def check_null(lambda_dict_in):
+    time1=time.time()
+
     lambda_dict= pickle.load(open(lambda_dict_in, "rb"))
-    list_in = list( itertools.chain(*lambda_dict.values() ))
+    print "lambda_dict loaded"
 
-    null_list=find_missed(list_coords=list_in, nside=2**9, theta_lim=pi/16)
-    
+    #list_in = list( itertools.chain(*lambda_dict.values() ))
+    list_in=lambda_dict[lambda_dict.keys()[0]]
+
+    null_list=find_missed(list_coords=list_in, nside=2**8, theta_lim=pi)    
     list2=list(map(tuple, null_list))
-    rad_plot_points(list2)
-    plt.show()
+    moll_plot_points(list2,"fullsky2", savename="allsky")
+    
 
+    time2=time.time()
+    print "Program Duration = %s" %(time2-time1)    
 
 if __name__=='__main__':
-    check_null('/home/rmkatti/skymapper/skymapper/data/lambda_dict_null_prac')
+    check_null('/data/rmkatti/lambda_dict_allsky_fullsky2')
--- skymapper/visualize/plot_points.py
+++ skymapper/visualize/plot_points.py
@@ -1,10 +1,14 @@
 """
 Short function for making radial (not deep sky) plots of sky pointings
 """
-
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
 
-def rad_plot_points(list_points):
+import numpy as np
+from numpy import pi
+
+def rad_plot_points(list_points, savename):
     """
     :param list_points, list of tuples of sky pointings 
         e.g. [(theta1,phi1,ax1), (theta2, phi2, ax2)...] 
@@ -18,4 +22,32 @@ def rad_plot_points(list_points):
     ax= plt.subplot(111, polar=True)
     scat= ax.scatter(phi_list, rad_list, s=10, edgecolors='none')
 
-    plt.show()
+    plt.savefig(savename+".png" )
+
+def moll_plot_points(list_points, plot_title, savename):
+    """
+    :param list_points, list of tuples of sky pointings 
+        e.g. [(theta1,phi1,ax1), (theta2, phi2, ax2)...] 
+        theta on [0,2*pi], phi on 0,2*pi
+    """
+
+    Nhits= len(list_points)
+    theta_vals, phi_vals = zip(*list_points)
+
+    Dec=pi/2-np.asarray(theta_vals)
+    RA= np.asarray(phi_vals)
+    RA[RA>pi]-= 2*pi    
+
+    fig= plt.figure()
+    ax= plt.subplot(111, projection="mollweide")
+    scat= ax.scatter(RA, Dec, s=5)
+    plt.grid(True)
+    plt.title(plot_title + "; Total Hits %s" %(Nhits), y=1.06, color='b')
+    fig.set_size_inches(20.0,10.0)
+
+    plt.savefig(savename+".png" )
+
+
+if __name__=='__main__':
+    moll_plot_points(list_points=[(theta,phi) for theta in np.linspace(0,pi,20) 
+    for phi in np.linspace(0,2*pi,5) ], plot_title="Test", savename='test' )
